884e62b57d0ff84504c1dea45ae162f80b5e6c27
Rylee Elkins <ryleeeliz@gmail.com>
2020-05-16 21:30:56 -0500

    Added contact form

---
+++ contactform.php
@@ -0,0 +1,17 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $tel = $_POST['tel'];
+    $message = $_POST['message'];
+
+    $mailto = "ryleeeliz@gmail.com"
+    $headers = "From: ".$email;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailto, $tel, $txt, $headers);
+    header("Location: index.html?mailsend");
+}
+
+?>
